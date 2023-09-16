ab1524c06f90b000647e6a137c9d8501bbab4b3f
Fatomi Abdul-Rahmon <114624223+Fatomi02@users.noreply.github.com>
2023-09-16 18:47:11 -0400

    Update [movie].js

--- src/components/[movie].js
+++ src/components/[movie].js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./movie.css";
 
 
@@ -6,6 +6,32 @@ const Movie = () => {
     let img_path="https://image.tmdb.org/t/p/w500"
     const data = JSON.parse(localStorage.getItem("movie-data"))
     console.log(data)
+
+    const [releaseDate, setReleaseDate] = useState("");
+    const [utcDate, setUtcDate] = useState("");
+
+    useEffect(() => {
+        if (data) {
+          //function to get release date in UTC
+          const getUTCTime = () => {
+            setReleaseDate(data.release_date);
+            const dates = releaseDate.split("-");
+    
+            if (dates.length === 3) {
+              const year = parseInt(dates[0]);
+              const month = parseInt(dates[1] - 1);
+              const day = parseInt(dates[2]);
+    
+              const date = new Date(Date.UTC(year, month, day));
+    
+              setUtcDate(date.toISOString());
+            }
+          };
+    
+          //call function
+          getUTCTime();
+        }
+      }, [data, releaseDate]);
 
     return (
     <>
@@ -17,10 +43,10 @@ const Movie = () => {
             <h2>{data?.title}</h2>
         </div>
         <div className="released-date" data-testid="movie-release-date">
-            {data?.release_date}
+        {utcDate}
         </div>
         <div className="runtime" data-testid="movie-runtime">
-
+        {data?.runtime}
         </div>
         <div className="overview" data-testid="movie-overview">
             <p>{data?.overview}</p>
